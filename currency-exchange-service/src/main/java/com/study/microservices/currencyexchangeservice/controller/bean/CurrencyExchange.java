5efb2ab8fef9dee7252f3477b0f431eca0bab2a6
Georgy Passos <georgy1101@gmail.com>
2021-06-22 17:27:04 -0300

    first commit

---
+++ currency-exchange-service/src/main/java/com/study/microservices/currencyexchangeservice/controller/bean/CurrencyExchange.java
@@ -0,0 +1,103 @@
+package com.study.microservices.currencyexchangeservice.controller.bean;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class CurrencyExchange {
+	
+	@Id
+	private Long id;
+	
+	@Column(name="currency_from")
+	private String from;
+	
+	@Column(name="currency_to")
+	private String to;
+	
+	private BigDecimal conversionMultiple;
+	
+	private String environment;
+
+	public CurrencyExchange() {
+		
+	} 
+
+	public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
+		this();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionMultiple = conversionMultiple;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public BigDecimal getConversionMultiple() {
+		return conversionMultiple;
+	}
+
+	public void setConversionMultiple(BigDecimal conversionMultiple) {
+		this.conversionMultiple = conversionMultiple;
+	}
+
+	public String getEnvironment() {
+		return environment;
+	}
+
+	public void setEnvironment(String environment) {
+		this.environment = environment;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CurrencyExchange other = (CurrencyExchange) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
